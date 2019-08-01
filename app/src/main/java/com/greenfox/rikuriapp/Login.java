81c6f17c2a52d23bcf0521f0218058b159f9ec64
Kris <kriskovacs90@gmail.com>
2019-08-01 23:36:09 +0200

    feat(otafe): add infoPage activity

--- app/src/main/java/com/greenfox/rikuriapp/Login.java
+++ app/src/main/java/com/greenfox/rikuriapp/Login.java
@@ -2,13 +2,31 @@ package com.greenfox.rikuriapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    Button infoPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        infoPage = (Button) findViewById(R.id.btnLogin2);
+        infoPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                infoPage();
+            }
+        });
+    }
+
+    public void infoPage() {
+        Intent intent = new Intent(this, InfoPage.class);
+        startActivity(intent);
     }
 }
