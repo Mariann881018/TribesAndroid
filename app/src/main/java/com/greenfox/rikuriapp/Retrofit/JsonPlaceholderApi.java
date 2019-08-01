fdcb5f7b12cc12747a460b458571dd80a34f1f70
foldestibor <foldestiborlajos@gmail.com>
2019-08-01 17:04:42 +0200

    feat(TA) add a get meth

--- app/src/main/java/com/greenfox/rikuriapp/Retrofit/JsonPlaceholderApi.java
+++ app/src/main/java/com/greenfox/rikuriapp/Retrofit/JsonPlaceholderApi.java
@@ -5,10 +5,15 @@ import com.greenfox.rikuriapp.Retrofit.registerdtos.UserDTO;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface JsonPlaceholderApi {
 
     @POST("/register")
     Call<ResponseDTO> postOnRegister(@Body UserDTO input);
+
+    @GET("/api//kingdom/building/{id}")
+    Call<> getKingdom(@Path ("id") Long id);
 }
---
+++ app/src/main/java/com/greenfox/rikuriapp/Retrofit/KingdomIdDto.java
@@ -0,0 +1,17 @@
+package com.greenfox.rikuriapp.Retrofit;
+
+public class KingdomIdDto {
+    private Long kingdomId;
+
+    public KingdomIdDto() {
+        this.kingdomId = kingdomId;
+    }
+
+    public Long getKingdomId() {
+        return kingdomId;
+    }
+
+    public void setKingdomId(Long kingdomId) {
+        this.kingdomId = kingdomId;
+    }
+}
